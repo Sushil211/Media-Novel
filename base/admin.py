1116e2fce79c0e809e16cb9889e50e6fcecaf42e
Sushil211 <sushilsekhar300@gmail.com>
2021-11-02 22:26:55 +0530

    complete project

---
+++ base/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . models import Room, Topic, Message
+
+# Register your models here. Qwerbnm67#
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
