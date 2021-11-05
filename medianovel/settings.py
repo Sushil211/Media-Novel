045913ce3ab714f9b04068cbb703d522e45af03b
Sushil211 <sushilsekhar300@gmail.com>
2021-11-05 18:17:33 +0530

    minor change in settings.py

--- medianovel/settings.py
+++ medianovel/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
